6d4c1068131e8a59e3909c70fce7fccb49c2c702
Alexander Slesarev <alex.slesarev@gmail.com>
2017-04-10 19:03:47 +1000

    Calculates the biggest integer N for N * lg(N) < expected number.

---
+++ nlng.scala
@@ -0,0 +1,39 @@
+#!/usr/bin/env scala
+
+object NLgNApp {
+  val usage = """
+    Usage: nlng <expected num>
+
+    Calculates the biggest integer N for N * lg(N) < expected number.
+    Multiplication operator is supported to evaluate the expected number, e.g.
+    nlng "60 * 1000000"
+  """
+
+  def main(args: Array[String]): Unit = {
+    if (args.length > 0) {
+      val lnOf2 = scala.math.log(2)
+      def log2(x: Double): Double = scala.math.log(x) / lnOf2
+      def fn(x: Double): Double = x * log2(x)
+
+      val expect = args(0).split('*').foldLeft(1.0)(_.toDouble * _.toDouble)
+      println(f"Max expected result: $expect%g")
+
+      var left = 0.0
+      var right = expect.toDouble
+
+      while ((right - left) > 1) {
+        val middle = (left + right) / 2
+        if (fn(middle) < expect) {
+          left = middle
+        } else {
+          right = middle
+        }
+      }
+
+      val result = left.floor
+      println(f"fn($result%g) = ${fn(result.toDouble)}%g")
+    } else {
+      println(usage)
+    }
+  }
+}
